67c605b26549623da26080bc504d247fa13f8918
Danil Zotin <danielzotin@gmail.com>
2023-06-15 12:05:37 +0300

    added comments

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/joho/godotenv"
 )
 
+// Setting up the button to ask for more messages
 var numericKeyboard = tgbotapi.NewReplyKeyboard(
 	tgbotapi.NewKeyboardButtonRow(
 		tgbotapi.NewKeyboardButton("еще!"),
@@ -17,15 +18,19 @@ var numericKeyboard = tgbotapi.NewReplyKeyboard(
 )
 
 func main() {
+	// Load the .env file, which contains the token
 	if err := godotenv.Load(); err != nil {
 		log.Println("No .env file found")
 	}
+	// Check if .env has ID_TOKEN field
 	token, found := os.LookupEnv("ID_TOKEN")
+	// Tell if it's found or not
 	if found {
 		log.Println("Token is found")
 	} else {
 		log.Println("Token is not found")
 	}
+	// Set up bot with token
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Panic(err)
@@ -33,11 +38,13 @@ func main() {
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
+	// Set up listener to updates
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
 	updates := bot.GetUpdatesChan(u)
 
+	// This block is needed to generate link like t.me/{channelName}/{randomNumberBetween{0}and{MaxIdMessage}}
 	channelName := ""
 	maxIdMessage := 0
 	link := "t.me/%s/%d"
@@ -46,8 +53,11 @@ func main() {
 		if update.Message == nil { // ignore non-Message updates
 			continue
 		}
-
+		
+		// msg is something we will send back to user
 		var msg tgbotapi.MessageConfig
+		// if user forwarded message from channel, we extract channel's @ and the id of forwarded message
+		// then we generate link and put it in txt value
 		if update.Message.ForwardFromChat != nil {
 			channelName = update.Message.ForwardFromChat.UserName
 			maxIdMessage = update.Message.ForwardFromMessageID
@@ -55,16 +65,19 @@ func main() {
 			msg = tgbotapi.NewMessage(update.Message.Chat.ID, txt)
 			msg.ReplyMarkup = numericKeyboard
 		} else if update.Message.Text == "еще!" {
+			// if user wants another random message, he sends "more" message and we repeat the generating of link
 			txt := fmt.Sprintf(link, channelName, rand.Intn(maxIdMessage))
 			msg = tgbotapi.NewMessage(update.Message.Chat.ID, txt)
 		} else {
-			//msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+			// if he does not send forward, we kindly remind user to do so
 			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Привет! Чтобы получить случайный пост с канала, сделай форвард из него в этот бот.")
 		}
 
+		// after succesfully creating msg, we send it to the user
 		if _, err := bot.Send(msg); err != nil {
 			log.Panic(err)
 		}
+		// this is needed to not send two identical messages at the same time
 		updates.Clear()
 	}
 }
