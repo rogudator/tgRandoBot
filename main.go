a380e363cd1eabec2f6b460b4591de2a29cddf40
Danil Zotin <danielzotin@gmail.com>
2023-08-11 02:01:11 +0300

    init

--- main.go
+++ main.go
@@ -1,101 +1,154 @@
 package main
 
 import (
-	"fmt"
+	"errors"
+	"os"
+
+	// "os"
+
+	//"fmt"
+	"io/ioutil"
 	"log"
 	"math/rand"
-	"os"
-	"sync"
+	"net/http"
+	"strconv"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
 )
 
-// Setting up the button to ask for more messages
+type html struct {
+	Body string `xml:"body"`
+}
+
+func GetLastPostId(channelName string) (int, error) {
+	url := "https://t.me/s/"
+	resp, err := http.Get(url + channelName)
+	if err != nil {
+		return 0, err
+	}
+
+	bodyByte, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return 0, err
+	}
+
+	bodyString := string(bodyByte)
+	beforeNumber := "href=\"https://t.me/" + channelName + "/"
+	trimBefore := strings.LastIndex(bodyString, beforeNumber)
+	lastPostString := ""
+
+	if trimBefore != -1 {
+		trimBefore += len(beforeNumber)
+		bodyString = bodyString[trimBefore:]
+		for _, v := range bodyString {
+			if v == '"' {
+				lastPostID, err := strconv.Atoi(lastPostString)
+				if err != nil {
+					return 0, err
+				}
+				return lastPostID, nil
+			}
+			lastPostString += string(v)
+		}
+
+	}
+	return 0, errors.New("Can't get id of channel's last post.")
+}
+
+func GetRandomPostLink(channelName string) (string, error) {
+	lastId, err := GetLastPostId(channelName)
+	if err != nil {
+		return "", err
+	}
+	// lastId := 14611
+	randoId := rand.Intn(lastId)
+	link := "t.me/" + channelName + "/" + strconv.Itoa(randoId)
+
+	return link, nil
+}
+
+func GetRandomPostID(channelName string) (int) {
+	randoId := 14
+	lastId, err := GetLastPostId(channelName)
+	if err != nil {
+		log.Println(err)
+		return rand.Intn(randoId)
+	}
+	randoId = rand.Intn(lastId)
+
+	return randoId
+}
+
 var numericKeyboard = tgbotapi.NewReplyKeyboard(
 	tgbotapi.NewKeyboardButtonRow(
-		tgbotapi.NewKeyboardButton("еще!"),
+		tgbotapi.NewKeyboardButton("moreeee"),
 	),
 )
 
+// init is invoked before main()
+func init() {
+    // loads values from .env into the system
+    if err := godotenv.Load(); err != nil {
+        log.Print("No .env file found")
+    }
+}
+
 func main() {
-	// Check if .env has ID_TOKEN field
-	token, found := os.LookupEnv("ID_TOKEN")
-	// Tell if it's found or not
-	if found {
-		log.Println("Token is found")
-	} else {
-		log.Fatal("Token is not found")
+	token, ok := os.LookupEnv("ID_TOKEN")
+	if !ok {
+		log.Fatal("couldn't read id token")
 	}
-	// Set up bot with token
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.Panic(err)
 	}
+	channel, ok := os.LookupEnv("CHANNEL")
+	if !ok {
+		log.Fatal("couldnt read channel name")
+	}
+	channelString, ok := os.LookupEnv("CHANNEL_ID")
+	if !ok {
+		log.Fatal("couldnt read channel id name")
+	}
+	channelID, err := strconv.ParseInt(channelString, 10, 64)
+	if err != nil {
+		log.Fatal("failed to convert env to valid id")
+	}
+
+	bot.Debug = false
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	// Set up listener to updates
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
 	updates := bot.GetUpdatesChan(u)
 
-	// This block is needed to generate link like t.me/{channelName}/{randomNumberBetween{0}and{MaxIdMessage}}
-	link := "t.me/%s/%d"
-	// This map is needed to remember which channel did user forward previously
-	var userAndChannelName sync.Map
-	// This map is needed to remember the id of post user forwarded previously
-	var userAndmaxIdMessage sync.Map
-	// In the long run, if the number of user grow too big. It will be a better
-	// idea to store this information in database.
+
 
 	for update := range updates {
 		if update.Message == nil { // ignore non-Message updates
 			continue
 		}
-		
-		// msg is something we will send back to user
-		var msg tgbotapi.MessageConfig
-		// if user forwarded message from channel, we extract channel's @ and the id of forwarded message
-		// then we generate link and put it in txt value
-		if update.Message.ForwardFromChat != nil {
-			userId := update.Message.Chat.ID
-			channelName := update.Message.ForwardFromChat.UserName
-			maxIdMessage := update.Message.ForwardFromMessageID
-			userAndChannelName.Store(userId, channelName)
-			userAndmaxIdMessage.Store(userId, maxIdMessage)
-			txt := fmt.Sprintf(link, channelName, rand.Intn(maxIdMessage))
-			msg = tgbotapi.NewMessage(userId, txt)
-			msg.ReplyMarkup = numericKeyboard
-		} else if update.Message.Text == "еще!" {
-			// if user wants another random message, he sends "more" message and we repeat the generating of link
-			txt := ""
-			userId := update.Message.Chat.ID
-			channelName, channelOk := userAndChannelName.Load(userId)
-			maxIdMessageAny, maxIdMessageOk := userAndmaxIdMessage.Load(userId)
-			if !channelOk || !maxIdMessageOk {
-				txt = "Чтобы получить случайный пост с канала, сделай форвард из него в этот бот."
-				msg = tgbotapi.NewMessage(update.Message.Chat.ID, txt)
-			} else {
-				maxIdMessage, ok := maxIdMessageAny.(int)
-				if !ok {
-					log.Println("Error when type asserting the maxIdMessage")
-					updates.Clear()
-					continue;
-				}
-				txt = fmt.Sprintf(link, channelName, rand.Intn(maxIdMessage))
-				msg = tgbotapi.NewMessage(update.Message.Chat.ID, txt)
-			}
-		} else {
-			// if he does not send forward, we kindly remind user to do so
-			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Привет! Чтобы получить случайный пост с канала, сделай форвард из него в этот бот.")
-		}
 
-		// after succesfully creating msg, we send it to the user
-		if _, err := bot.Send(msg); err != nil {
-			log.Panic(err)
+		forwardIsNotSent := true
+		for forwardIsNotSent {
+			postID := GetRandomPostID(channel)
+			msg2 := tgbotapi.NewForward(update.Message.Chat.ID , channelID, postID) 
+			_, err := bot.Send(msg2)
+			if err == nil {
+				forwardIsNotSent = false
+			}
+			log.Println(postID)
 		}
-		// this is needed to not send two identical messages at the same time
+		var msg tgbotapi.MessageConfig
+		msg.Text = "Another post?"
+		msg.ChatID = update.Message.Chat.ID
+		msg.ReplyMarkup = numericKeyboard
+		
+		bot.Send(msg)
 		updates.Clear()
 	}
 }
